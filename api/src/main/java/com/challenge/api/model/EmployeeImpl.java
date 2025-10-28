4afc5cc2ecbe74544d82ee1b6a493716ca6ec006
HungHong <Honghuyhung46@gmail.com>
2025-10-28 18:58:51 -0400

    Adding required file
    Adding required file for EmployeeImpl and service

---
+++ api/src/main/java/com/challenge/api/model/EmployeeImpl.java
@@ -0,0 +1,7 @@
+package com.challenge.api.model;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class EmployeeImpl {
+}
---
+++ api/src/main/java/com/challenge/api/service/EmployeeService.java
@@ -0,0 +1,5 @@
+package com.challenge.api.service;
+
+public class EmployeeService {
+
+}
