e1b2b90afb6407a3abfd06266c22c62aa20bfa3c
HungHong <176441814+HungHHong@users.noreply.github.com>
2025-10-28 20:10:35 -0400

    implement required method, add mock data

--- api/src/main/java/com/challenge/api/service/EmployeeService.java
+++ api/src/main/java/com/challenge/api/service/EmployeeService.java
@@ -1,31 +1,45 @@
 package com.challenge.api.service;
+
 import com.challenge.api.model.EmployeeImpl;
+import org.springframework.stereotype.Service;
 import java.util.*;
 
     /**
-     * Service layer responsible for handling all Employee related operations.
-     *
-     * TODO:
-     *  - Store employees in a local in-memory data structure
-     *  - Implement methods to:
-     *      1. getAllEmployees()
-     *      2. getEmployeeByUuid(UUID uuid)
-     *      3. createEmployee(EmployeeImpl employee)
-     *
-     * This class will later be used by EmployeeController to perform CRUD operations.
+     * Handles core business logic for managing employees.
+     * Stores employees in an in-memory map and provides simple CRUD-style methods.
      */
 
 public class EmployeeService {
     private final Map<UUID, EmployeeImpl> employeeStore = new HashMap<>();
 
+    /** Mock Data for testing purposes */
+    public EmployeeService() {
+        createEmployee(new EmployeeImpl(
+                null, "Hong", "H", 85000, 30, "Software Engineer",
+                "Hong.H@quest.com", Instant.now(), null));
+
+        createEmployee(new EmployeeImpl(
+                null, "Bob", "Johnson", 92000, 35, "DevOps Engineer",
+                "bob.johnson@gmail.com", Instant.now(), null));
+    }
+
+
+    // --------------------------------------------------------
+    // Implement required method
+    // --------------------------------------------------------
+
+    // return all employee currently store
     public List<EmployeeImpl> getAllEmployees() {
         return new ArrayList<>(employeeStore.values());
     }
 
+    //retrieves employee by their unique UUID
     public EmployeeImpl getEmployeeByUuid(UUID uuid) {
         return employeeStore.get(uuid);
     }
 
+
+    //Create and stores a new employee. Generates a UUID if doesn't exist
     public EmployeeImpl createEmployee(EmployeeImpl employee) {
         if (employee.getUuid() == null) {
             employee.setUuid(UUID.randomUUID());
